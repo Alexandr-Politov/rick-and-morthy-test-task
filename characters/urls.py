6414d0e36ef685ad5c1678984729d1b9af667cd2
Alexandr Politov <apolitov83@gmail.com>
2024-11-24 13:24:29 +0200

    added 'character-list' endpoint with filtering by name

--- characters/urls.py
+++ characters/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 
-from characters.views import get_random_character
+from characters.views import get_random_character, CharacterListView
 
 
 app_name = "characters"
 
-urlpatterns = [path("characters/random", get_random_character, name="character-random")]
+urlpatterns = [
+    path("characters/random", get_random_character, name="character-random"),
+    path("characters/", CharacterListView.as_view(), name="character-list"),
+]
--- characters/views.py
+++ characters/views.py
@@ -1,5 +1,6 @@
 import random
 
+from django.db.models import QuerySet
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -17,3 +18,19 @@ def get_random_character(request: Request) -> Response:
     random_character = Character.objects.get(pk=random_pk)
     serializer = CharacterSerializer(random_character)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharacterListView(generics.ListAPIView):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self) -> QuerySet:
+        """
+        Optionally restricts the returned purchases to a given character,
+        by filtering against a `name` query parameter in the URL.
+        """
+        queryset = super().get_queryset()
+        name = self.request.query_params.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
